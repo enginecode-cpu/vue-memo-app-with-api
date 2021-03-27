e26fbbb7ad37c78df5b51e2327fb01b5b7bfab72
enginecode-cpu <bassmatics1002@gmail.com>
2021-03-28 01:11:22 +0900

    feat: implement methods using vuex actions

--- src/components/MemoApp.vue
+++ src/components/MemoApp.vue
@@ -14,47 +14,24 @@
 </template>
 
 <script>
-import axios from "axios";
 import MemoForm from "./MemoForm";
 import Memo from "./Memo";
-
-const memoAPICore = axios.create({
-  baseURL: "http://localhost:3000/api/todo"
-});
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "MemoApp",
-  data: () => {
-    return {
-      memos: []
-    };
-  },
   components: {
     MemoForm,
     Memo
   },
+  computed: {
+    ...mapState(["memos"])
+  },
   created() {
-    memoAPICore.get("/").then(res => (this.memos = res.data));
+    this.fetchMemos();
   },
   methods: {
-    addMemo(payload) {
-      const { title, content } = payload;
-      memoAPICore.post("/", payload).then(res => this.memos.push(res.data));
-    },
-    deleteMemo(id) {
-      const targetIndex = this.memos.findIndex(v => v.id === id);
-      memoAPICore
-        .delete(`/${id}`)
-        .then(() => this.memos.splice(targetIndex, 1));
-    },
-    updateMemo(payload) {
-      const { id, content } = payload;
-      const targetIndex = this.memos.findIndex(v => v.id === id);
-      const targetMemo = this.memos[targetIndex];
-      memoAPICore.patch(`/${id}`, { content }).then(() => {
-        this.memos.splice(targetIndex, 1, { ...targetMemo, content });
-      });
-    }
+    ...mapActions(["fetchMemos", "addMemo", "deleteMemo", "updateMemo"])
   }
 };
 </script>
