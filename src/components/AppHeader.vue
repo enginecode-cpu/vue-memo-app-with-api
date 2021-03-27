7e2d42ebb04d94b6506c3ac1e5bb057c7fad79fd
enginecode-cpu <bassmatics1002@gmail.com>
2021-03-28 01:06:25 +0900

    feat: implement counting functions using getters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <h1 class="app-header">메모 애플리케이션</h1>
+  <div class="app-header">
+    <h1>메모 애플리케이션</h1>
+    <p>
+      <strong>{{ getMemoCount }}</strong>
+    </p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  computed: {
+    ...mapGetters(["getMemoCount"])
+  }
 };
 </script>
 
@@ -14,11 +21,19 @@ export default {
 .app-header {
   overflow: hidden;
   padding: 52px 0 27px;
-
   h1 {
     float: left;
     font-size: 24px;
     text-align: center;
+  }
+  p {
+    float: right;
+    padding-top: 0.5rem;
+    font-size: 18px;
+    vertical-align: bottom;
+  }
+  p strong {
+    color: #ff5a00;
   }
 }
 </style>
