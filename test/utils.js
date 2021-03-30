bae11af196317217da65540ec8bd5d2369292eb3
enginecode-cpu <bassmatics1002@gmail.com>
2021-03-30 23:37:34 +0900

    feat: make test utils module

---
+++ test/utils.js
@@ -0,0 +1,4 @@
+export const getByTag = (wrapper, tag) => wrapper.find(tag);
+export const getByClassName = (wrapper, className) => wrapper.find(className);
+export const getById = (wrapper, id) => wrapper.find(id);
+export const getBySelector = (wrapper, selector) => wrapper.find(selector);
